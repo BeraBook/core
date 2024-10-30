203aa178f2fdbe69df2355555113d63d3073b9d7
Sean <detectivekim1@gmail.com>
2024-10-30 17:24:09 +0900

    feat: deploy

--- deploy/BookManager.ts
+++ deploy/BookManager.ts
@@ -1,8 +1,7 @@
 import { HardhatRuntimeEnvironment } from 'hardhat/types'
 import { DeployFunction } from 'hardhat-deploy/types'
 import { getChain, isDevelopmentNetwork } from '@nomicfoundation/hardhat-viem/internal/chains'
-import { deployCreate3WithVerify, deployWithVerify } from '../utils'
-import { base } from 'viem/chains'
+import { deployWithVerify } from '../utils'
 import { Address } from 'viem'
 
 const deployFunction: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
@@ -23,17 +22,12 @@ const deployFunction: DeployFunction = async function (hre: HardhatRuntimeEnviro
   let defaultProvider: Address = '0x'
   if (chain.testnet || isDevelopmentNetwork(chain.id)) {
     owner = defaultProvider = deployer
-  } else if (chain.id === base.id) {
-    owner = '0xfb976Bae0b3Ef71843F1c6c63da7Df2e44B3836d' // Safe
-    defaultProvider = '0xfc5899d93df81ca11583bee03865b7b13ce093a7' // Treasury
   } else {
     throw new Error('Unknown chain')
   }
 
-  const entropy = 1000n
-  await deployCreate3WithVerify(
-    deployer,
-    entropy,
+  await deployWithVerify(
+    hre,
     'BookManager',
     [
       owner,
--- hardhat.config.ts
+++ hardhat.config.ts
@@ -5,9 +5,6 @@ import * as dotenv from 'dotenv'
 import readlineSync from 'readline-sync'
 
 import 'hardhat-deploy'
-import '@matterlabs/hardhat-zksync-deploy'
-import '@matterlabs/hardhat-zksync-solc'
-import '@matterlabs/hardhat-zksync-verify'
 import '@nomicfoundation/hardhat-viem'
 import '@nomicfoundation/hardhat-foundry'
 import '@nomicfoundation/hardhat-verify'
@@ -55,12 +52,7 @@ const loadPrivateKeyFromKeyfile = () => {
     }
   }
 
-  const prodNetworks = new Set<number>([
-    networkInfos.mainnet.id,
-    networkInfos.arbitrum.id,
-    networkInfos.base.id,
-    networkInfos.zkSync.id,
-  ])
+  const prodNetworks = new Set<number>([networkInfos.mainnet.id, networkInfos.arbitrum.id, networkInfos.base.id])
   if (network && prodNetworks.has(network)) {
     if (privateKey) {
       return privateKey
@@ -83,16 +75,6 @@ const loadPrivateKeyFromKeyfile = () => {
 }
 
 const config: HardhatConfig = {
-  zksolc: {
-    version: 'latest', // Uses latest available in https://github.com/matter-labs/zksolc-bin/
-    settings: {
-      libraries: {
-        'src/libraries/Book.sol': {
-          Book: '0xAc742Cf41d12fA3835f2c658897D5D64a02eCEF8',
-        },
-      },
-    },
-  },
   solidity: {
     compilers: [
       {
@@ -123,40 +105,6 @@ const config: HardhatConfig = {
       saveDeployments: true,
       tags: ['testnet', 'test'],
       companionNetworks: {},
-    },
-    [networkInfos.zkSyncSepoliaTestnet.id]: {
-      url: networkInfos.zkSyncSepoliaTestnet.rpcUrls.default.http[0],
-      chainId: networkInfos.zkSyncSepoliaTestnet.id,
-      accounts: process.env.DEV_PRIVATE_KEY ? [process.env.DEV_PRIVATE_KEY] : [],
-      gas: 'auto',
-      gasPrice: 'auto',
-      gasMultiplier: 1,
-      timeout: 3000000,
-      httpHeaders: {},
-      live: true,
-      saveDeployments: true,
-      tags: ['testnet', 'test'],
-      companionNetworks: {},
-      ethNetwork: 'sepolia', // The Ethereum Web3 RPC URL, or the identifier of the network (e.g. `mainnet` or `sepolia`)
-      verifyURL: 'https://explorer.sepolia.era.zksync.dev/contract_verification',
-      zksync: true,
-    },
-    [networkInfos.zkSync.id]: {
-      url: networkInfos.zkSync.rpcUrls.default.http[0],
-      chainId: networkInfos.zkSync.id,
-      accounts: [loadPrivateKeyFromKeyfile()],
-      gas: 'auto',
-      gasPrice: 'auto',
-      gasMultiplier: 1,
-      timeout: 3000000,
-      httpHeaders: {},
-      live: true,
-      saveDeployments: true,
-      tags: ['mainnet', 'prod'],
-      companionNetworks: {},
-      ethNetwork: 'mainnet', // The Ethereum Web3 RPC URL, or the identifier of the network (e.g. `mainnet` or `sepolia`)
-      verifyURL: 'https://zksync2-mainnet-explorer.zksync.io/contract_verification',
-      zksync: true,
     },
     [networkInfos.berachainTestnetbArtio.id]: {
       url: networkInfos.berachainTestnetbArtio.rpcUrls.default.http[0],
@@ -290,7 +238,7 @@ const config: HardhatConfig = {
         network: networkInfos.berachainTestnetbArtio.id.toString(),
         chainId: networkInfos.berachainTestnetbArtio.id,
         urls: {
-          apiURL: 'https://api.routescan.io/v2/network/testnet/evm/80084/etherscan/api/',
+          apiURL: 'https://api.routescan.io/v2/network/testnet/evm/80084/etherscan/',
           browserURL: 'https://bartio.beratrail.io',
         },
       },
